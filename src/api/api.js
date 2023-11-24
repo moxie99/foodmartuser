6b065e3d454a328b9b76b2d8bc3e5eb57bc7eca0
Adeolu David <adeolusegun99@gmail.com>
2023-11-24 16:31:41 +0100

    minor base url changes

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const local = process.env.LOCAL_BASE_URL;
-const production = process.env.PRODUCTION_BASE_URL;
+const production = 'https://foodmart-o1ja.onrender.com';
 const api = axios.create({
   baseURL: `${production}/api`,
   withCredentials: true,
